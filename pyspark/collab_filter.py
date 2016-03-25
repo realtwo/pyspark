f43c7a00cad0a659a0917521c92880e564706215
= <fred.chou.nd@gmail.com>
2016-03-25 19:13:33 +0800

    updated collab filtering

--- pyspark/collab_filter.py
+++ pyspark/collab_filter.py
@@ -9,6 +9,7 @@ from pyspark import SparkConf, SparkContext
 from math import sqrt
 
 user_id = 2
+item_limit = 2
 
 conf = SparkConf().setMaster("local").setAppName("APrioriAlgo")
 sc = SparkContext(conf = conf)
@@ -32,9 +33,13 @@ rowMatRdd.persist()
 def getRow(rowMatRdd, idx):
 	return rowMatRdd.filter(lambda x: x[0]==idx).map(lambda x: x[1])
 
+def getCol(rowMatRdd, idx): 
+	return rowMatRdd.map(lambda x: x[1][idx])
+
 def calNorm(row):
 	return sqrt(sum(row))
 
+#item-item based collaborate filter
 userRow = getRow(rowMatRdd, user_id-1).first()
 userNorm = calNorm(userRow)
 
@@ -50,5 +55,39 @@ for i in xrange(0, m):
 
 print userSim
 
+estRating =[]  # user-user
+for idx_col in xrange(0, item_limit):
+	col = getCol(rowMatRdd, idx_col).collect()
+	print col
+	s = 0
+	for j in xrange(0, m):
+		s+=userSim[j]*col[j]
+	estRating.append(s)
+
+print "predicted rating based on user-user collab filter: "
+print estRating
+
+#item-item based collaborate filter
+estRating2 = []  #item-item 
+for idx_col in xrange(0, item_limit):
+	itemCol = getCol(rowMatRdd, idx_col).collect()
+	itemNorm = calNorm(itemCol)
+	itemSim =[]
+	for j in xrange(0, n):
+		col = getCol(rowMatRdd, j).collect()
+		colNorm = calNorm(col)
+		s = 0
+		for k in xrange(0, m):
+			s+=itemCol[k]*col[k]
+		itemSim.append(s/itemNorm/colNorm)
+	print itemSim
+
+	s = 0
+	for j in xrange(0, n):
+		s+=itemSim[j]*userRow[j]
+	estRating2.append(s)
+
+print "predicted rating based on item-item collab filter: "
+print estRating2
 
 
